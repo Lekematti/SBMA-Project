65bec37f59964b9d2f173c204de1715279a3e033
Bibek <study.bibekshrestha@gmail.com>
2024-03-05 23:53:42 +0200

    buttons fixed

--- app/src/main/java/com/example/sbma_project/views/History.kt
+++ app/src/main/java/com/example/sbma_project/views/History.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,9 +19,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -176,14 +179,18 @@ fun History(timerViewModel: TimerViewModel) {
                         onClick = {
                             timerViewModel.deleteTimerById(timerIdToDelete.value)
                             showDialog.value = false
-                        }
+                        },
+                        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
+                        contentPadding = PaddingValues(16.dp)
                     ) {
                         Text("Confirm")
                     }
                 },
                 dismissButton = {
                     Button(
-                        onClick = { showDialog.value = false }
+                        onClick = { showDialog.value = false },
+                        colors = ButtonDefaults.outlinedButtonColors(),
+                        contentPadding = PaddingValues(16.dp)
                     ) {
                         Text("Cancel")
                     }
